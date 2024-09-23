0fd563b24e8c2c45d9ea58f857c43addb401bd4d
minahkim03 <minahkim026@gmail.com>
2024-09-23 13:10:37 +0900

    0923

---
+++ spring-study/js/api.js
@@ -0,0 +1,45 @@
+import axios from 'axios'
+import Cookies from 'universal-cookie'
+import jwtAxios from './customAxios'
+
+export const testApi = () => {
+    console.log("test Api...")
+}
+
+const url = "http://localhost:8080/api/v1/"
+const cookies = new Cookies(null, {path: '/', maxAge: 2592000})
+
+export const makeToken = async (mid, mpw) => {
+    const path = url + "token/make"
+    const data = {mid, mpw}
+    const res = await axios.post(path, data)
+    return res.data
+}
+
+export const saveToken = (tokenName, tokenValue) => {
+    cookies.set(tokenName, tokenValue)
+}
+
+export const getSamples = async (pageNum) => {
+    const path = url + "samples/list"
+    const res = await jwtAxios.get(path)
+    return res.data
+}
+
+export async function requestRefreshToken(){
+    const refreshToken = cookies.get("refreshToken")
+    const mid = cookies.get("mid")
+    const accessToken = cookies.get("accessToken")
+
+    if(!mid || !refreshToken || !accessToken) {
+        throw Error("Cannot request refresh..")
+    }
+
+    const path = url + "token/refresh"
+    const header = {'content-type' : 'application/x-www-form-urlencoded', 'Authorization' : 'Bearer ' + accessToken}
+    const data = {refreshToken, mid}
+    const res = await axios.post(path, data, {headers:header})
+
+    return res.data
+}
+
